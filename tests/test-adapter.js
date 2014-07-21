2c95b376fa3d3ecf4f115a3420bbafb45474addf
Gordon Mickel <gm@cyon.ch>
2014-07-21 07:38:38 +0200

    add console logging to test

--- tests/test-adapter.js
+++ tests/test-adapter.js
@@ -13,5 +13,11 @@ test('Test functionality of adapter', function(t) {
 test('Test start of adapter', function(t) {
   var instance = new Adapter({'track': ['funny', 'hilarious', 'gif', 'cat']});
   instance.start();
+  instance.on('gif', function(url) {
+    console.log('url', url);
+    instance.stop();
+    t.ok(url);
+    t.end();
+  });
   t.end();
 });
